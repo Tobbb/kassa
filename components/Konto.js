e6f9227ac9593a7098589d92b7beb7cab21a181a
Tobias Bernström <tobias_be@hotmail.com>
2019-07-18 15:44:33 +0200

    small change

--- components/Konto.js
+++ components/Konto.js
@@ -34,7 +34,7 @@ export default class Konto extends React.Component {
             style={styles.bt}
             title="Stäng fönster"
           accessibilityLabel="Stäng"
-          color="#ff8800"
+          color="#ff0000"
         />
         </View>
     );
